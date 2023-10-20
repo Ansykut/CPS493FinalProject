77fda128266c68cba94df53cba9c4f8388d46cf1
Ansykut <ansykut@gmail.com>
2023-10-20 16:01:52 -0400

    Friends Activity working local

--- client/src/views/PeopleSearchView.vue
+++ client/src/views/PeopleSearchView.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="about">
-      <h1>This is an Friends actvity page</h1>
+      <h1>This is an People Search page
+        It should always be visible to all users
+      </h1>
     </div>
   </template>
   
